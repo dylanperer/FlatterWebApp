42805f77a9043a3199bbf7d18dc914674bbaae0f
dylanperer <dylan.perer29@protonmail.com>
2023-10-18 23:01:50 +1300

    .

--- src/components/virtualized/Range.ts
+++ src/components/virtualized/Range.ts
@@ -1,124 +1,118 @@
-import {
-    createMemo,
-    createRoot,
-    getOwner,
-    JSX,
-    untrack
-} from "solid-js";
+import { createMemo, createRoot, getOwner, JSX, untrack } from "solid-js";
 
 export function mapRange<T>(
-    rangeStart: () => number,
-    rangeCount: () => number,
-    mapFn: (i: number) => T,
-    options: { fallback?: () => any } = {}
+   rangeStart: () => number,
+   rangeCount: () => number,
+   mapFn: (i: number) => T,
+   options: { fallback?: () => any } = {}
 ): () => T[] {
-    const ctx = getOwner()!;
-    let prevItems: T[];
-    let prevDisposers: (() => void)[] = [];
-    let prevStart = 0;
-    let prevEnd = 0;
-    let prevCount = 0;
+   const ctx = getOwner()!;
+   let prevItems: T[];
+   let prevDisposers: (() => void)[] = [];
+   let prevStart = 0;
+   let prevEnd = 0;
+   let prevCount = 0;
 
-    return () => {
-        const start = rangeStart();
-        const count = rangeCount();
+   return () => {
+      const start = rangeStart();
+      const count = rangeCount();
 
-        if (count < 0) {
-            return prevItems || null;
-        }
+      if (count < 0) {
+         return prevItems || null;
+      }
 
-        return untrack(() => {
-            const end = start + count;
-            const arrSize = count || options.fallback ? 1 : 0;
-            const items = new Array<T>(arrSize);
-            const disposers = new Array<() => void>(arrSize);
-            let i: number;
+      return untrack(() => {
+         const end = start + count;
+         const arrSize = count || options.fallback ? 1 : 0;
+         const items = new Array<T>(arrSize);
+         const disposers = new Array<() => void>(arrSize);
+         let i: number;
 
-            function mapper(disposer: () => void) {
-                disposers[i] = disposer;
-                return mapFn(i + start);
+         function mapper(disposer: () => void) {
+            disposers[i] = disposer;
+            return mapFn(i + start);
+         }
+
+         if (!prevCount) {
+            if (count > 0) {
+               if (prevDisposers.length) {
+                  prevDisposers[0]();
+               }
+               for (i = 0; i < count; i++) {
+                  items[i] = createRoot(mapper, ctx);
+               }
+            } else if (prevItems) {
+               return prevItems;
             }
-
-            if (!prevCount) {
-                if (count > 0) {
-                    if (prevDisposers.length) {
-                        prevDisposers[0]();
-                    }
-                    for (i = 0; i < count; i++) {
-                        items[i] = createRoot(mapper, ctx);
-                    }
-                } else if (prevItems) {
-                    return prevItems;
-                }
-            } else if (!count) {
-                for (i = 0; i < prevCount; i++) {
-                    prevDisposers[i]();
-                    if (options.fallback) {
-                        items[0] = createRoot((disposer) => {
-                            disposers[0] = disposer;
-                            return options.fallback!();
-                        }, ctx);
-                    }
-                }
-            } else if (start >= prevEnd || end <= prevStart) {
-                for (i = 0; i < Math.min(count, prevCount); i++) {
-                    prevDisposers[i]();
-                    items[i] = createRoot(mapper, ctx);
-                }
-                for (; i < prevCount; i++) {
-                    prevDisposers[i]();
-                }
-                for (; i < count; i++) {
-                    items[i] = createRoot(mapper, ctx);
-                }
-            } else {
-                const offset = prevStart - start;
-                for (
-                    i = Math.max(0, offset);
-                    i < Math.min(count, prevCount + offset);
-                    i++
-                ) {
-                    items[i] = prevItems[i - offset];
-                    disposers[i] = prevDisposers[i - offset];
-                }
-                for (i = 0; i < start - prevStart; i++) {
-                    prevDisposers[i]();
-                }
-                for (i = end - prevStart; i < prevEnd - prevStart; i++) {
-                    prevDisposers[i]();
-                }
-                for (i = 0; i < prevStart - start; i++) {
-                    items[i] = createRoot(mapper, ctx);
-                }
-                for (i = prevEnd - start; i < end - start; i++) {
-                    items[i] = createRoot(mapper, ctx);
-                }
+         } else if (!count) {
+            for (i = 0; i < prevCount; i++) {
+               prevDisposers[i]();
+               if (options.fallback) {
+                  items[0] = createRoot((disposer) => {
+                     disposers[0] = disposer;
+                     return options.fallback!();
+                  }, ctx);
+               }
             }
+         } else if (start >= prevEnd || end <= prevStart) {
+            for (i = 0; i < Math.min(count, prevCount); i++) {
+               prevDisposers[i]();
+               items[i] = createRoot(mapper, ctx);
+            }
+            for (; i < prevCount; i++) {
+               prevDisposers[i]();
+            }
+            for (; i < count; i++) {
+               items[i] = createRoot(mapper, ctx);
+            }
+         } else {
+            const offset = prevStart - start;
+            for (
+               i = Math.max(0, offset);
+               i < Math.min(count, prevCount + offset);
+               i++
+            ) {
+               items[i] = prevItems[i - offset];
+               disposers[i] = prevDisposers[i - offset];
+            }
+            for (i = 0; i < start - prevStart; i++) {
+               prevDisposers[i]();
+            }
+            for (i = end - prevStart; i < prevEnd - prevStart; i++) {
+               prevDisposers[i]();
+            }
+            for (i = 0; i < prevStart - start; i++) {
+               items[i] = createRoot(mapper, ctx);
+            }
+            for (i = prevEnd - start; i < end - start; i++) {
+               items[i] = createRoot(mapper, ctx);
+            }
+         }
 
-            prevDisposers = disposers;
-            prevItems = items;
-            prevStart = start;
-            prevEnd = end;
-            prevCount = count;
+         prevDisposers = disposers;
+         prevItems = items;
+         prevStart = start;
+         prevEnd = end;
+         prevCount = count;
 
-            return items;
-        });
-    };
+         return items;
+      });
+   };
 }
 
 export default function Range<U extends JSX.Element>(props: {
-    start: number;
-    count: number;
-    fallback?: JSX.Element;
-    children: (item: number) => U;
+   start: number;
+   count: number;
+   fallback?: JSX.Element;
+   children: (item: number) => U;
 }) {
-    const fallback = "fallback" in props && { fallback: () => props.fallback };
-    return createMemo(
-        mapRange<U>(
-            () => props.start,
-            () => props.count,
-            props.children,
-            fallback ? fallback : undefined
-        )
-    );
+   const fallback = "fallback" in props && { fallback: () => props.fallback };
+   return createMemo(
+      mapRange<U>(
+         () => props.start,
+         () => props.count,
+         props.children,
+         fallback ? fallback : undefined
+      )
+   );
 }
--- src/components/virtualized/Virtualize.ts
+++ src/components/virtualized/Virtualize.ts
@@ -1,101 +1,104 @@
 import { createComputed, createMemo } from "solid-js";
-import {createStore} from "solid-js/store";
+import { createStore } from "solid-js/store";
 
 export interface VirtualizeProps {
-    size: number;
-    itemSize: number;
-    totalCount: number;
-    position: number;
-    buffer?: number;
+   size: number;
+   itemSize: number;
+   totalCount: number;
+   position: number;
+   buffer?: number;
 }
 
 export interface Virtualized {
-    start: number;
-    end: number;
-    count: number;
-    margins: [number, number];
-    dimension: number;
+   start: number;
+   end: number;
+   count: number;
+   margins: [number, number];
+   dimension: number;
 }
 
 export function createVirtualized(
-    size: () => number,
-    itemSize: () => number,
-    totalCount: () => number,
-    position: () => number,
-    buffer?: () => number | undefined
+   size: () => number,
+   itemSize: () => number,
+   totalCount: () => number,
+   position: () => number,
+   buffer?: () => number | undefined
 ): Virtualized {
-    const [state, setState] = createStore({
-        start: 0,
-        end: 0,
-        count: 0
-    });
+   const [state, setState] = createStore({
+      start: 0,
+      end: 0,
+      count: 0,
+   });
 
-    const visibleRange = createMemo(
-        () => {
-            const size_ = size();
-            const itemSize_ = itemSize();
-            if (size_ <= 0 || itemSize_ <= 0) {
-                return [0, 0];
-            }
-            const start = Math.floor(position() / itemSize_);
-            return [start, start + Math.max(Math.ceil(size_ / itemSize_), 1)];
-        },
-        undefined,
-        //@ts-ignore
-        tuplesEqual
-    );
+   const visibleRange = createMemo(
+      () => {
+         const size_ = size();
+         const itemSize_ = itemSize();
+         if (size_ <= 0 || itemSize_ <= 0) {
+            return [0, 0];
+         }
+         const start = Math.floor(position() / itemSize_);
+         return [start, start + Math.max(Math.ceil(size_ / itemSize_), 1)];
+      },
+      undefined,
+      //@ts-ignore
+      tuplesEqual
+   );
 
-    createComputed(() => {
-        const buffer_ = orDefault(buffer, 1);
-        const [visStart, visEnd] = visibleRange();
-        const start = Math.max(visStart - buffer_, 0);
-        const end = Math.min(visEnd + buffer_, totalCount());
-        setState({
-            start,
-            end,
-            count: end - start
-        });
-    });
+   createComputed(() => {
+      const buffer_ = orDefault(buffer, 1);
+      const [visStart, visEnd] = visibleRange();
+      const start = Math.max(visStart - buffer_, 0);
+      const end = Math.min(visEnd + buffer_, totalCount());
+      setState({
+         start,
+         end,
+         count: end - start,
+      });
+   });
 
-    const margins = createMemo<[number, number]>(
-        () => {
-            const itemSize_ = itemSize();
-            const { start, end } = state;
-            return [start * itemSize_, (totalCount() - end) * itemSize_];
-        },
-        //@ts-ignore
-        undefined,
-        tuplesEqual
-    );
+   const margins = createMemo<[number, number]>(
+      () => {
+         const itemSize_ = itemSize();
+         const { start, end } = state;
+         return [start * itemSize_, (totalCount() - end) * itemSize_];
+      },
+      //@ts-ignore
+      undefined,
+      tuplesEqual
+   );
 
+   const dimension = createMemo(
+      () => state.count * itemSize(),
+      undefined,
+       //@ts-ignore
+      true
+   );
 
-    //@ts-ignore
-    const dimension = createMemo(() => state.count * itemSize(), undefined, true);
-
-    return {
-        get start() {
-            return state.start;
-        },
-        get end() {
-            return state.end;
-        },
-        get count() {
-            return state.count;
-        },
-        get margins() {
-            return margins();
-        },
-        get dimension() {
-            return dimension();
-        }
-    };
+   return {
+      get start() {
+         return state.start;
+      },
+      get end() {
+         return state.end;
+      },
+      get count() {
+         return state.count;
+      },
+      get margins() {
+         return margins();
+      },
+      get dimension() {
+         return dimension();
+      },
+   };
 }
 
 function tuplesEqual<T extends any[]>(a: [...T], b: [...T]) {
-    return a.every((item, index) => item === b[index]);
+   return a.every((item, index) => item === b[index]);
 }
 
 function orDefault<T>(fn: (() => T | undefined) | undefined, fallback: T) {
-    const value = fn ? fn() : undefined;
-    return value !== undefined ? value : fallback;
+   const value = fn ? fn() : undefined;
+   return value !== undefined ? value : fallback;
 }
